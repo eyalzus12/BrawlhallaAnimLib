736d9944976273405c30de98bbf0849b895c21a4
EyalZusiman <eyal.zusiman@gmail.com>
2025-03-26 17:53:43 +0200

    implement spawn bot type reading

---
+++ src/Reading/SpawnBotTypesGfx.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using BrawlhallaAnimLib.Bones;
+using BrawlhallaAnimLib.Gfx;
+
+namespace BrawlhallaAnimLib.Reading;
+
+public sealed class SpawnBotTypesGfx
+{
+    internal string AnimClass { get; }
+    internal string AnimFile { get; }
+    internal bool UseTrueLeftRightTorso { get; } = false;
+    internal uint AsymmetrySwapFlags { get; } = 0;
+    internal List<InternalCustomArtImpl> CustomArts { get; } = [];
+    internal List<InternalColorSwapImpl> ColorSwaps { get; } = [];
+
+    public SpawnBotTypesGfx(XElement element)
+    {
+        foreach (XElement child in element.Elements())
+        {
+            string key = child.Name.LocalName;
+            string value = child.Value;
+            if (key == "Gfx")
+            {
+                foreach (XElement prop in child.Elements())
+                {
+                    string propKey = prop.Name.LocalName;
+                    string propValue = prop.Value;
+                    if (propKey == "AsymmetrySwapFlags")
+                    {
+                        uint asf = propValue.Split(",").Select(static (flag) =>
+                        {
+                            if (Enum.TryParse(flag, out BoneTypeEnum result))
+                                return 1u << (int)result;
+                            return 0u;
+                        }).Aggregate((a, v) => a | v);
+
+                        AsymmetrySwapFlags = asf;
+                    }
+                    else if (propKey.StartsWith("CustomArt"))
+                    {
+                        CustomArts.Add(ParserUtils.ParseCustomArt(propValue, true, ArtTypeEnum.Companion));
+                    }
+                    else if (propKey == "AnimFile")
+                    {
+                        AnimFile = propValue;
+                    }
+                    else if (propKey == "AnimClass")
+                    {
+                        AnimClass = propValue;
+                    }
+                }
+            }
+            else if (key == "UseTrueLeftRightTorso")
+            {
+                UseTrueLeftRightTorso = ParserUtils.ParseBool(value);
+            }
+        }
+
+        if (AnimClass is null) throw new ArgumentException("Missing AnimClass");
+        if (AnimFile is null) throw new ArgumentException("Missing AnimFile");
+    }
+
+    public IGfxType ToGfxType()
+    {
+        InternalGfxImpl gfxResult = new()
+        {
+            AnimFile = AnimFile,
+            AnimClass = AnimClass,
+            AsymmetrySwapFlags = AsymmetrySwapFlags,
+            UseTrueLeftRightTorso = UseTrueLeftRightTorso,
+            CustomArtsInternal = [.. CustomArts],
+            ColorSwapsInternal = [.. ColorSwaps],
+        };
+
+        return gfxResult;
+    }
+}
